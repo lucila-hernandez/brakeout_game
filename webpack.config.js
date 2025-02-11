18169baf5d715534752082ea84be3bb5c484f9ff
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-02-11 14:43:15 -0800

    removed dupliate js files fro m GitHub repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,7 @@
-/* eslint-disable no-undef */
+const path = require('path');
+
 module.exports = {
-  entry: './src/main.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
